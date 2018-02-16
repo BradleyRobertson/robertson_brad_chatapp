2f3dbbe9a45cf499da349df58e01b21d091d6d70
Your Name <you@example.com>
2018-02-15 19:24:00 -0500

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -11,29 +11,29 @@
     nickName = this.value;
   }
 
-  function handleSendMessage(e) {
-    e.preventDefault();
-   nickName = (nickName && nickName.length > 0) ? nickName : 'user';
-   msg = `${nickName} says ${chatMessage.value}`;
-
-   socket.emit('chat message', msg);
-   checkMessage.value = "";
-   return false;
-}
-
   function appendMessage(msg) {
   let newMsg = `<li>${msg.message}</li>`;
   messageList.innerHTML += newMsg;
   }
 
-  function appendDiscMessage(msg) {
+  function appendDescMessage(msg) {
     let newMsg = `<li>${msg}</li>`;
     messageList.innerHTML += newMsg;
   }
+	
+	  function handleSendMessage(e) {
+    e.preventDefault();
+   nickName = ( nickName && nickName.length > 0) ? nickName : 'user';
+   msg = `${nickName} says ${chatMessage.value}`;
+
+   socket.emit('chat message', msg);
+   chatMessage.value = "";
+   return false;
+}
 
 
-  nameInput.addEventListener('chamge', setNickname, false);
+  nameInput.addEventListener('change', setNickname, false);
   chatForm.addEventListener('submit', handleSendMessage, false);
   socket.addEventListener('chat message', appendMessage, false);
-  socket.addEventListener('disconnect message', appendDiscMessage, false);
+  socket.addEventListener('disconnect message', appendDescMessage, false);
 })();
